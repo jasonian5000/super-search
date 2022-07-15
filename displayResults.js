365c79d09600f06f764876dfda2d704a80b35526
Jason <jason.blunck@gmail.com>
2022-07-14 22:43:29 -0500

    finished search page and results page, added about me page and made all pages mobile friendly

--- displayResults.js
+++ displayResults.js
@@ -1,16 +1,16 @@
 const cv_key = config.CV_API_KEY;
 const tmdb_key = config.TMDB_KEY;
+const charURL = "https://www.comicvine.com/api/characters/";
 const searchResult = JSON.parse(localStorage.getItem("char"));
 
 const displayChar = () => {
-    document.getElementById("charName").innerText = searchResult.name
-    document.getElementById("charImage").src = searchResult.image.medium_url
-    document.getElementById("charSummary").innerText = searchResult.deck
-    document.getElementById("charLink").href = searchResult.site_detail_url
-    console.log(searchResult)
+  document.getElementById("charName").innerText = searchResult.name;
+  document.getElementById("charImage").src = searchResult.image.medium_url;
+  document.getElementById("charSummary").innerText = searchResult.deck;
+  document.getElementById("charLink").href = searchResult.site_detail_url;
 };
 
-displayChar()
+displayChar();
 
 let charFull = JSON.parse(localStorage.getItem("charList"));
 
@@ -21,8 +21,7 @@ const charDetails = async () => {
   const json = await result.json();
   charFull = await json.results;
   localStorage.setItem("charList", JSON.stringify(json.results));
-  console.log(charFull);
-    charMovies();
+  charMovies();
 };
 
 let movieList = [];
@@ -38,8 +37,6 @@ const charMovies = () => {
   createMoviePanels(movieList);
 };
 
-// setTimeout(function () {charDetails()}, 1001);
-
 let movieName = "";
 let movieResults = [];
 const moviePanels = document.getElementById("movie-panels");
@@ -48,12 +45,9 @@ const movieSearch = async (movieName) => {
   let encodeMovieName = encodeURIComponent(movieName);
   let posterPathURL = "http://image.tmdb.org/t/p/original";
   let url = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb_key}&query=${encodeMovieName}&include_adult=false`;
-  //   let url = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb_key}&query=Batman&include_adult=false`;
-  // let url = `https://api.themoviedb.org/3/movie/560057/watch/providers?api_key=${tmdb_key}&language=en-US`
   const result = await fetch(url);
   const json = await result.json();
   movieResults = await json.results;
-  console.log(movieName);
   if (movieResults.length > 0) {
     for (let index = 0; index < movieResults.length; index++) {
       if (
@@ -71,13 +65,12 @@ const movieSearch = async (movieName) => {
     }
   }
   if (movieResults.length > 0) {
-    var imageDiv = `<li><div class="image_title"><a href="#">${movieName}</a></div>
-                    <a href=""><img src=${posterPathURL}${movieResults[0].poster_path} border="0"></a></li>`;
+    var tmdbLink = `https://www.themoviedb.org/movie/${movieResults[0].id}`;
+    var imageDiv = `<li><div class="image_title"><a href="${tmdbLink}" target="_blank">${movieName}</a></div>
+                    <a href="${tmdbLink}" target="_blank"><img src=${posterPathURL}${movieResults[0].poster_path} border="0"></a></li>`;
     moviePanels.insertAdjacentHTML("beforeend", imageDiv);
-    console.log(movieResults);
   }
 };
-// movieSearch("Batman");
 
 const createMoviePanels = (movieList) => {
   for (let movie = 0; movie < movieList.length; movie++) {
@@ -85,11 +78,59 @@ const createMoviePanels = (movieList) => {
     movieSearch(movieName);
   }
 };
-// charMovies()
 
-const leftButton = document.getElementById("left-button")
-const rightButton = document.getElementById("right-button")
-leftButton.onmousedown = () => {document.getElementById("movie-panels").scrollBy({left: -100, behavior: 'smooth'});
-console.log("left")}
-rightButton.onmousedown = () => {document.getElementById("movie-panels").scrollBy({left: 100, behavior: 'smooth'})}
-// setTimeout(function () {charDetails()}, 1001);
+const leftButton = document.getElementById("left-button");
+const rightButton = document.getElementById("right-button");
+leftButton.onclick = () => {
+  document
+    .getElementById("movie-panels")
+    .scrollBy({ left: -224, behavior: "smooth" });
+};
+rightButton.onclick = () => {
+  document
+    .getElementById("movie-panels")
+    .scrollBy({ left: 224, behavior: "smooth" });
+};
+setTimeout(function () {
+  charDetails();
+}, 1001);
+
+const getChar = async () => {
+  const inputText = searchInput.value.toLowerCase();
+  let messageArea = document.getElementById("message-area");
+  let fieldList = "name,deck,image,site_detail_url,api_detail_url";
+  if (inputText) {
+    let url = `${charURL}?api_key=${cv_key}&filter=name:${inputText}&field_list=${fieldList}&format=json`;
+    try {
+      const charList = await fetch(url);
+      const json = await charList.json();
+      let searchResults = json.results;
+      if (searchResults.length === 0) {
+        messageArea.innerText = "Try a new search.";
+      }
+      for (let index = 0; index < searchResults.length; index++) {
+        if (searchResults[index].name.toLowerCase() === inputText) {
+          localStorage.setItem("char", JSON.stringify(searchResults[index]));
+          window.open("./displayResults.html", "_self");
+          break;
+        } else {
+          localStorage.setItem("char", JSON.stringify(searchResults[0]));
+          window.open("./displayResults.html", "_self");
+        }
+      }
+    } catch (e) {
+      console.error(e);
+      messageArea.innerText = "Try again later.";
+    }
+    let charFull = JSON.parse(localStorage.getItem("charList"));
+  }
+};
+
+const searchInput = document.getElementById("search-input");
+
+searchInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    getChar();
+  }
+});
